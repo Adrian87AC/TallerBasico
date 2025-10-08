0e80bd2d60dedc7406b6bfeabd566c77b9ed970e
Adrián.Álvarez <adrian.alvarez@eusa.es>
2025-10-08 09:36:07 +0200

    Add HTML and JavaScript exercises for basic arithmetic operations and decision making

---
+++ front/js/actividades.js
@@ -0,0 +1,10 @@
+/*Ejercicio 1: Variables y operaciones aritméticas básicas
+1. Crea una variable llamada a y asígnale el valor 10.
+2. Crea otra variable llamada b y asígnale el valor 5.
+3. Suma las dos variables y guarda el resultado en una variable llamada suma.
+4. Resta b a a y guarda el resultado en una variable llamada resta.
+5. Multiplica a por b y guarda el resultado en una variable llamada multiplicacion.
+6. Divide a entre b y guarda el resultado en una variable llamada division.
+7. Muestra todos los resultados (suma, resta, multiplicacion, division) en la
+consola.
+*/
